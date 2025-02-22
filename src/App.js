330077a7093fcdb30764443e380715fe4523c897
dhrxuv <dhruvqwerty3@gmail.com>
2025-02-22 13:17:37 +0530

    update landing page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Landingpage from './pages/landingpage'
 import './App.css';
 import Imagecapsule from './pages/imagecapsule';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
     <Imagecapsule/>
+    <Landingpage/>
     </div>
   );
 }
